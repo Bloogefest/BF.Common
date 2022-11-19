4ffd1a82a4944495bcbaadfaf335a2f287c48acb
Bloogefest <bloogefest@gmail.com>
2022-11-19 21:32:25 +0300

    Добавление исключения интерфейса Function

---
+++ src/com/bloogefest/common/function/ExecuteException.java
@@ -0,0 +1,72 @@
+package com.bloogefest.common.function;
+
+/**
+ * @author Bloogefest
+ * @version 0.0
+ * @since 0.0.1
+ */
+public class ExecuteException extends FunctionException {
+
+    /**
+     * Конструктор, использующий стандартное сообщение.
+     *
+     * @author Bloogefest
+     * @since 0.0.1
+     */
+    public ExecuteException() {
+        super();
+    }
+
+    /**
+     * Конструктор, использующий сообщение, переданное параметром.
+     *
+     * @param message Сообщение, используемое вместо стандартного.
+     *
+     * @author Bloogefest
+     * @since 0.0.1
+     */
+    public ExecuteException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Конструктор, использующий причину, переданную параметром.
+     *
+     * @param cause Причина возникновения исключения.
+     *
+     * @author Bloogefest
+     * @since 0.0.1
+     */
+    public ExecuteException(final Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Конструктор, использующий сообщение и причину, переданные параметрами.
+     *
+     * @param message Сообщение, используемое вместо стандартного.
+     * @param cause   Причина возникновения исключения.
+     *
+     * @author Bloogefest
+     * @since 0.0.1
+     */
+    public ExecuteException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Конструктор, использующий сообщение, причину, разрешение об упоминании подавленных исключений и разрешение для записи трассировки стека, переданные параметрами.
+     *
+     * @param message     Сообщение, используемое вместо стандартного.
+     * @param cause       Причина возникновения исключения.
+     * @param suppression Позволяет упоминать подавленные исключения.
+     * @param writable    Позволяет трассировке стека записываться.
+     *
+     * @author Bloogefest
+     * @since 0.0.1
+     */
+    protected ExecuteException(final String message, final Throwable cause, final boolean suppression, final boolean writable) {
+        super(message, cause, suppression, writable);
+    }
+
+}
