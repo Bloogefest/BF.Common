4f7b4dd5f7cd85963247e3738fe5fedc163194d7
Bloogefest <bloogefest@gmail.com>
2023-02-18 20:19:05 +0300

    Модуляризация проекта

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * Модуль утилитарной библиотеки.
+ *
+ * @since 0.3.0
+ */
+module com.bloogefest.common {
+
+    requires transitive org.jetbrains.annotations;
+
+    exports com.bloogefest.common.base;
+    exports com.bloogefest.common.creation;
+    exports com.bloogefest.common.creation.builder;
+    exports com.bloogefest.common.environment;
+    exports com.bloogefest.common.function;
+    exports com.bloogefest.common.validation;
+
+}
