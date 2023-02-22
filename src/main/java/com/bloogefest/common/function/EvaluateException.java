7e895421f46e023f699e094198e5d0ee42694df3
Bloogefest <bloogefest@gmail.com>
2023-02-22 21:18:05 +0300

    Введение класса `EvaluateException`

---
+++ src/main/java/com/bloogefest/common/function/EvaluateException.java
@@ -0,0 +1,102 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.bloogefest.common.function;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Класс исключения оценивания экземпляра.
+ *
+ * @since 3.0
+ */
+public class EvaluateException extends FunctionException {
+
+    /**
+     * Инициализирует экземпляр по умолчанию.
+     *
+     * @since 3.0
+     */
+    @Contract(pure = true)
+    public EvaluateException() {
+        super();
+    }
+
+    /**
+     * Инициализирует экземпляр с переопределённым сообщением.
+     *
+     * @param message сообщение.
+     *
+     * @since 3.0
+     */
+    @Contract(pure = true)
+    public EvaluateException(final @NonNls @Nullable String message) {
+        super(message);
+    }
+
+    /**
+     * Инициализирует экземпляр с переопределённой причиной.
+     *
+     * @param cause причина.
+     *
+     * @since 3.0
+     */
+    @Contract(pure = true)
+    public EvaluateException(final @Nullable Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Инициализирует экземпляр с переопределённым сообщением и причиной.
+     *
+     * @param message сообщение.
+     * @param cause причина.
+     *
+     * @since 3.0
+     */
+    @Contract(pure = true)
+    public EvaluateException(final @NonNls @Nullable String message, final @Nullable Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Инициализирует экземпляр с переопределённым параметром подавления и трассировки стека.
+     *
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 3.0
+     */
+    @Contract(pure = true)
+    public EvaluateException(final boolean suppression, final boolean writable) {
+        super(suppression, writable);
+    }
+
+    /**
+     * Инициализирует экземпляр с переопределённым сообщением, причиной, параметром подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 3.0
+     */
+    @Contract(pure = true)
+    protected EvaluateException(final @NonNls @Nullable String message, final @Nullable Throwable cause,
+                                final boolean suppression, final boolean writable) {
+        super(message, cause, suppression, writable);
+    }
+
+}
