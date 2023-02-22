1c5cfa76abb5b2e8bb78548a3335017b14be32c3
Bloogefest <bloogefest@gmail.com>
2023-02-22 12:46:17 +0300

    Рефакторинг класса `Validator`

--- src/main/java/com/bloogefest/common/validation/Validator.java
+++ src/main/java/com/bloogefest/common/validation/Validator.java
@@ -8,6 +8,7 @@ package com.bloogefest.common.validation;
 
 import com.bloogefest.common.creation.UtilityException;
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,11 +31,73 @@ public final class Validator {
     }
 
     /**
+     * Проверяет и возвращает экземпляр, если он нулевой, в противном случае генерирует исключение.
+     *
+     * @param instance экземпляр.
+     *
+     * @return Нулевой экземпляр.
+     *
+     * @throws NotNullException исключение валидации ненулевого экземпляра.
+     *
      * @since 1.0
      */
-    public static <T, E> @NotNull T notNull(@Nullable T o, @NotNull Object... ignored) {
-        if (o == null) throw new NullException();
-        return o;
+    @Contract(value = "!null -> fail; _ -> null", pure = true)
+    public static <T> @Nullable T isNull(final @Nullable T instance) throws NotNullException {
+        if (instance != null) throw new NotNullException();
+        return null;
+    }
+
+    /**
+     * Проверяет и возвращает экземпляр, если он нулевой, в противном случае генерирует исключение.
+     *
+     * @param instance экземпляр.
+     * @param name имя экземпляра.
+     *
+     * @return Нулевой экземпляр.
+     *
+     * @throws NotNullException исключение валидации ненулевого экземпляра.
+     *
+     * @since 1.0
+     */
+    @Contract(value = "!null, _ -> fail; _, _ -> null", pure = true)
+    public static <T> @Nullable T isNull(final @Nullable T instance, final @NonNls @NotNull String name) throws NotNullException {
+        if (instance != null) throw new NotNullException(NotNullException.templateMessage.formatted(name));
+        return null;
+    }
+
+    /**
+     * Проверяет и возвращает экземпляр, если он ненулевой, в противном случае генерирует исключение.
+     *
+     * @param instance экземпляр.
+     *
+     * @return Ненулевой экземпляр.
+     *
+     * @throws NullException исключение валидации нулевого экземпляра.
+     *
+     * @since 1.0
+     */
+    @Contract(value = "!null -> param1; null -> fail", pure = true)
+    public static <T> @NotNull T notNull(final @Nullable T instance) throws NullException {
+        if (instance == null) throw new NullException();
+        return instance;
+    }
+
+    /**
+     * Проверяет и возвращает экземпляр, если он ненулевой, в противном случае генерирует исключение.
+     *
+     * @param instance экземпляр.
+     * @param name имя экземпляра.
+     *
+     * @return Ненулевой экземпляр.
+     *
+     * @throws NullException исключение валидации нулевого экземпляра.
+     *
+     * @since 1.0
+     */
+    @Contract(value = "!null, _ -> param1; null, _ -> fail", pure = true)
+    public static <T> @NotNull T notNull(final @Nullable T instance, final @NonNls @NotNull String name) throws NullException {
+        if (instance == null) throw new NullException(NullException.templateMessage.formatted(name));
+        return instance;
     }
 
 }
