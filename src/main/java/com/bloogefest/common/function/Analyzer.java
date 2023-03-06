6305af03f51b808d6540629d0755a51573d8cb8f
Bloogefest <bloogefest@gmail.com>
2023-03-06 20:38:22 +0300

    Рефакторинг интерфейса `Analyzer<T, R>`

--- src/main/java/com/bloogefest/common/function/Analyzer.java
+++ src/main/java/com/bloogefest/common/function/Analyzer.java
@@ -4,6 +4,18 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.bloogefest.common.function;
 
 import com.bloogefest.common.validation.NullException;
@@ -13,46 +25,31 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Функциональный интерфейс анализатора объектов.
+ * Функциональный интерфейс анализатора объекта.
  *
  * @param <T> тип анализируемого объекта.
+ * @param <R> тип результирующего объекта.
  *
  * @since 3.0
  */
 @FunctionalInterface
-public interface Analyzer<T> {
+public interface Analyzer<T, R> {
 
     /**
-     * Инициализирует анализатор без логики.
+     * Проверяет результирующий объект и, если он ненулевой, инициализирует анализатор, метод анализа которого всегда
+     * возвращает его, в противном случае инициализирует и бросает исключение.
      *
-     * @return Анализатор без логики.
+     * @param object результирующий объект.
      *
+     * @return Анализатор с постоянным результирующим объектом.
+     *
+     * @throws NullException исключение валидации нулевого результирующего объекта.
      * @since 3.0
      */
-    @Contract(value = "-> new", pure = true)
-    static <T> @NotNull Analyzer<T> empty() {
-        return ignored -> {};
-    }
-
-    /**
-     * Проверяет переданный анализатор и анализируемый объект и, если они ненулевые, инициализирует другой анализатор,
-     * метод анализа которого всегда вызывает метод анализа переданного анализатора, используя переданный анализируемый
-     * объект, в противном случае инициализирует и бросает исключение.
-     *
-     * @param analyzer анализатор.
-     * @param object анализируемый объект.
-     *
-     * @return Анализатор с постоянным анализируемым объектом.
-     *
-     * @throws NullException исключение валидации нулевого анализатора либо нулевого анализируемого объекта.
-     * @since 3.0
-     */
-    @Contract(value = "!null, !null -> new; _, _ -> fail", pure = true)
-    static <T> @NotNull Analyzer<T> constant(final @Nullable Analyzer<T> analyzer,
-                                             final @Nullable T object) throws NullException {
-        Validator.notNull(analyzer, "analyzer");
+    @Contract(value = "!null -> new; _ -> fail", pure = true)
+    static <T, R> @NotNull Analyzer<T, R> constant(final @Nullable R object) throws NullException {
         Validator.notNull(object, "object");
-        return ignored -> analyzer.analyze(object);
+        return ignored -> object;
     }
 
     /**
@@ -68,60 +65,40 @@ public interface Analyzer<T> {
      * @since 3.0
      */
     @Contract(value = "!null -> param1; _ -> fail", pure = true)
-    static <T> @NotNull Analyzer<T> of(final @Nullable Analyzer<T> analyzer) throws NullException {
+    static <T, R> @NotNull Analyzer<T, R> of(final @Nullable Analyzer<T, R> analyzer) throws NullException {
         return Validator.notNull(analyzer, "analyzer");
     }
 
     /**
-     * Проверяет анализатор и, если он ненулевой, возвращает его, в противном случае инициализирует и возвращает другой
-     * анализатор без логики.
-     *
-     * @param analyzer анализатор.
-     *
-     * @return Переданный либо другой ненулевой анализатор.
-     *
-     * @apiNote Этот метод можно использовать для приведения лямбда-выражений к типу анализатора.
-     * @since 3.0
-     */
-    @Contract(value = "!null -> param1; _ -> new", pure = true)
-    static <T> @NotNull Analyzer<T> auto(final @Nullable Analyzer<T> analyzer) {
-        return analyzer != null ? analyzer : empty();
-    }
-
-    /**
-     * Анализирует объект.
+     * Анализирует переданный объект, инициализирует результирующий объект и возвращает его.
      *
      * @param object анализируемый объект.
      *
+     * @return Результирующий объект.
+     *
      * @throws NullException исключение валидации нулевого объекта.
-     * @throws AnalyzeException исключение анализа объекта.
+     * @throws HandleException исключение анализа объекта.
      * @since 3.0
      */
     @Contract
-    void analyze(final @NotNull T object) throws NullException, AnalyzeException;
+    @NotNull R analyze(final @NotNull T object) throws NullException, HandleException;
 
     /**
      * Проверяет переданный анализатор и, если он ненулевой, инициализирует другой анализатор, метод анализа которого
      * последовательно соединяет метод анализа этого и переданного анализатора, используя конструкцию try-finally, в
      * противном случае инициализирует и бросает исключение.
      *
-     * @param analyzer анализатор.
+     * @param analyzer объект анализатора.
      *
      * @return Анализатор с комбинированной логикой этого и переданного анализатора.
      *
      * @throws NullException исключение валидации нулевого анализатора.
      * @since 3.0
      */
-    @Contract(value = "!null -> new; _ -> fail", pure = true)
-    default @NotNull Analyzer<T> with(final @Nullable Analyzer<? super T> analyzer) throws NullException {
+    @Contract(value = "_ -> new", pure = true)
+    default <R_> @NotNull Analyzer<T, R_> with(final @Nullable Analyzer<? super R, R_> analyzer) throws NullException {
         Validator.notNull(analyzer, "analyzer");
-        return object -> {
-            try {
-                analyze(object);
-            } finally {
-                analyzer.analyze(object);
-            }
-        };
+        return object -> analyzer.analyze(analyze(object));
     }
 
 }
