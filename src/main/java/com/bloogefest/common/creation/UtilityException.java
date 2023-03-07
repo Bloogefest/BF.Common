a316d843a99f25571b774080fcf4807acb204215
Bloogefest <bloogefest@gmail.com>
2023-03-07 16:00:19 +0300

    Рефакторинг поля `UtilityException.defaultMessage`

--- src/main/java/com/bloogefest/common/creation/UtilityException.java
+++ src/main/java/com/bloogefest/common/creation/UtilityException.java
@@ -8,6 +8,7 @@ package com.bloogefest.common.creation;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -22,7 +23,7 @@ public class UtilityException extends CreationException {
      *
      * @since 3.0
      */
-    protected static final @NonNls @Nullable String defaultMessage = "The utility object must not be created";
+    public static final @NonNls @NotNull String defaultMessage = "The utility object must not be created";
 
     /**
      * Шаблонное сообщение.
