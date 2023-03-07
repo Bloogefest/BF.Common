4c0c684d4944450c2d7559d69148ecd38f44b286
Bloogefest <bloogefest@gmail.com>
2023-03-07 15:45:28 +0300

    Рефакторинг поля `CallException.defaultMessage`

--- src/main/java/com/bloogefest/common/function/CallException.java
+++ src/main/java/com/bloogefest/common/function/CallException.java
@@ -8,6 +8,7 @@ package com.bloogefest.common.function;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -22,7 +23,7 @@ public class CallException extends FunctionException {
      *
      * @since 3.0
      */
-    protected static final @NonNls @Nullable String defaultMessage = "The function could not be called";
+    public static final @NonNls @NotNull String defaultMessage = "The function could not be called";
 
     /**
      * Шаблонное сообщение.
