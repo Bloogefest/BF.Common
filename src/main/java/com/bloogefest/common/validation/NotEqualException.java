bc1515a085948e614dc7cb91755fd54ff73f1176
Bloogefest <bloogefest@gmail.com>
2023-03-07 15:54:25 +0300

    Рефакторинг поля `NotEqualException.templateMessage`

--- src/main/java/com/bloogefest/common/validation/NotEqualException.java
+++ src/main/java/com/bloogefest/common/validation/NotEqualException.java
@@ -30,7 +30,7 @@ public class NotEqualException extends ValidationException {
      *
      * @since 2.0
      */
-    protected static final @NonNls @NotNull String templateMessage = "The %s must be equal to the %s";
+    public static final @NonNls @NotNull String templateMessage = "The %s must be equal to the %s";
 
     /**
      * Инициализирует исключение по умолчанию.
