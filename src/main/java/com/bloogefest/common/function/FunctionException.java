b3cca9b4dc812f249087fb742d47d5ebf8efdf57
Bloogefest <bloogefest@gmail.com>
2023-03-07 15:53:35 +0300

    Рефакторинг поля `FunctionException.templateMessage`

--- src/main/java/com/bloogefest/common/function/FunctionException.java
+++ src/main/java/com/bloogefest/common/function/FunctionException.java
@@ -31,7 +31,7 @@ public class FunctionException extends SoftException {
      *
      * @since 3.0
      */
-    protected static final @NonNls @Nullable String templateMessage = "The %s could not be executed";
+    public static final @NonNls @NotNull String templateMessage = "The %s could not be executed";
 
     /**
      * Инициализирует исключение по умолчанию.
