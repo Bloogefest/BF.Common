f7c26fd2753c7d4d397634ee93a85fb22126e509
Bloogefest <bloogefest@gmail.com>
2023-03-09 22:17:23 +0300

    Введение метода `Predicate::otherwise(Supplier<Throwable>)`

--- src/main/java/com/bloogefest/common/function/Predicate.java
+++ src/main/java/com/bloogefest/common/function/Predicate.java
@@ -169,4 +169,16 @@ public interface Predicate<T> {
         };
     }
 
+    /**
+     * @since 3.1
+     */
+    @Contract(value = "_ -> new", pure = true)
+    default @NotNull Predicate<T> otherwise(final @NotNull Supplier<Throwable> supplier) throws NullException {
+        Validator.notNull(supplier, "supplier");
+        return object -> {
+            if (!evaluate(object)) throw new ComputeException(supplier.supply());
+            return true;
+        };
+    }
+
 }
