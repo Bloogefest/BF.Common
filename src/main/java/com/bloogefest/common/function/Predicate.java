fa510672cc49ddab86d3d220438facfd6f9f740d
Bloogefest <bloogefest@gmail.com>
2023-03-11 19:37:43 +0300

    Рефакторинг сигнатуры метода `Predicate::and(Predicate)`

--- src/main/java/com/bloogefest/common/function/Predicate.java
+++ src/main/java/com/bloogefest/common/function/Predicate.java
@@ -90,7 +90,7 @@ public interface Predicate<T> {
      * @since 1.0
      */
     @Contract(value = "_ -> new", pure = true)
-    default @NotNull Predicate<T> and(final @NotNull Predicate<T> predicate) throws NullException {
+    default @NotNull Predicate<T> and(final @NotNull Predicate<? super T> predicate) throws NullException {
         Validator.notNull(predicate, "predicate");
         return object -> evaluate(object) && predicate.evaluate(object);
     }
