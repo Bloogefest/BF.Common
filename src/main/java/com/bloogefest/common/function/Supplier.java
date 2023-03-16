94542350c45b02b5e06264735e1667d8da151b8d
Bloogefest <bloogefest@gmail.com>
2023-03-16 23:54:24 +0300

    Рефакторинг аннотаций метода `Supplier::supply()`

--- src/main/java/com/bloogefest/common/function/Supplier.java
+++ src/main/java/com/bloogefest/common/function/Supplier.java
@@ -64,7 +64,7 @@ public interface Supplier<T> {
      * @throws SupplyException исключение поставки объекта.
      * @since 1.0.0
      */
-    @Contract(pure = true)
+    @Contract
     @NotNull T supply() throws SupplyException;
 
 }
