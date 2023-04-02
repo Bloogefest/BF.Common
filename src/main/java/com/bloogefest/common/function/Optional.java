fb9d92437a48813c040efc95cf0d50aa02ebf87b
Bloogefest <bloogefest@gmail.com>
2023-04-02 12:39:39 +0300

    Введение интерфейса `Optional`

---
+++ src/main/java/com/bloogefest/common/function/Optional.java
@@ -0,0 +1,20 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.bloogefest.common.function;
+
+/**
+ * Функциональный интерфейс контейнера объекта.
+ *
+ * @param <T> тип объекта.
+ *
+ * @apiNote Позволяет безопасно работать с обнуляемыми объектами.
+ * @since 4.0.0
+ */
+@FunctionalInterface
+public interface Optional<T> {
+
+}
