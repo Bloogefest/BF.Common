4b329a3d6307f1a68a65f9cb01159fb0fe11e432
Bloogefest <bloogefest@gmail.com>
2023-04-02 20:04:23 +0300

    Введение исключения `GetException`

---
+++ src/main/java/com/bloogefest/common/function/GetException.java
@@ -0,0 +1,111 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.bloogefest.common.function;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Исключение получения объекта.
+ *
+ * @since 4.0.0
+ */
+public class GetException extends FunctionException {
+
+    /**
+     * Сообщение по умолчанию.
+     *
+     * @since 4.0.0
+     */
+    public static final @NonNls @NotNull String defaultMessage = "Failed to get the object";
+
+    /**
+     * Шаблонное сообщение.
+     *
+     * @since 4.0.0
+     */
+    public static final @NonNls @NotNull String templateMessage = "Failed to get the %s";
+
+    /**
+     * Инициализирует исключение по умолчанию.
+     *
+     * @since 4.0.0
+     */
+    @Contract(pure = true)
+    public GetException() {
+        super(defaultMessage);
+    }
+
+    /**
+     * Инициализирует исключение с переопределённым сообщением.
+     *
+     * @param message сообщение.
+     *
+     * @since 4.0.0
+     */
+    @Contract(pure = true)
+    public GetException(final @NonNls @Nullable String message) {
+        super(message);
+    }
+
+    /**
+     * Инициализирует исключение с переопределённой причиной.
+     *
+     * @param cause причина.
+     *
+     * @since 4.0.0
+     */
+    @Contract(pure = true)
+    public GetException(final @Nullable Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Инициализирует исключение с переопределённым сообщением и причиной.
+     *
+     * @param message сообщение.
+     * @param cause причина.
+     *
+     * @since 4.0.0
+     */
+    @Contract(pure = true)
+    public GetException(final @NonNls @Nullable String message, final @Nullable Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Инициализирует исключение с переопределённым параметром подавления и трассировки стека.
+     *
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0
+     */
+    @Contract(pure = true)
+    public GetException(final boolean suppression, final boolean writable) {
+        super(suppression, writable);
+    }
+
+    /**
+     * Инициализирует исключение с переопределённым сообщением, причиной, параметром подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0
+     */
+    @Contract(pure = true)
+    protected GetException(final @NonNls @Nullable String message, final @Nullable Throwable cause,
+                           final boolean suppression, final boolean writable) {
+        super(message, cause, suppression, writable);
+    }
+
+}
