bbe61b5b55030f758cf50a6f186da99710c91dbb
Bloogefest <bloogefest@gmail.com>
2023-06-05 13:07:54 +0300

    Изменение полей и конструкторов исключения `CreationException`: `CreationException.defaultMessage`, `CreationException.templateMessage`, `CreationException()`, `CreationException(String)`, `CreationException(Throwable)`, `CreationException(String, Throwable)`, `CreationException(boolean, boolean)`, `CreationException(String, Throwable, boolean, boolean)` —, а также введение других: `CreationException.DEFAULT_MESSAGE`, `CreationException.TEMPLATE_MESSAGE`, `CreationException(String, boolean, boolean)`, `CreationException(Throwable, boolean, boolean)`

--- src/main/java/com/bloogefest/common/creation/CreationException.java
+++ src/main/java/com/bloogefest/common/creation/CreationException.java
@@ -6,10 +6,7 @@
 
 package com.bloogefest.common.creation;
 
-import com.bloogefest.annotation.analysis.Contract;
-import com.bloogefest.annotation.analysis.NotNls;
-import com.bloogefest.annotation.analysis.NotNull;
-import com.bloogefest.annotation.analysis.Nullable;
+import com.bloogefest.annotation.analysis.*;
 import com.bloogefest.common.SoftException;
 
 /**
@@ -22,79 +19,127 @@ public class CreationException extends SoftException {
     /**
      * Сообщение по умолчанию.
      *
-     * @since 3.0.0
+     * @since 4.0.0-RC3
      */
-    public static final @NotNls @NotNull String defaultMessage = "The object could not be created";
+    public static final @NotNls @NotNull String DEFAULT_MESSAGE = "Failed to create the object";
+
+    /**
+     * Шаблонное сообщение.
+     *
+     * @since 4.0.0-RC3
+     */
+    public static final @NotNls @NotNull String TEMPLATE_MESSAGE = "Failed to create the %s";
+
+    /**
+     * Сообщение по умолчанию.
+     *
+     * @since 3.0.0
+     * @deprecated Используйте {@linkplain #DEFAULT_MESSAGE}.
+     */
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.CreationException.DEFAULT_MESSAGE")
+    public static final @NotNls @NotNull String defaultMessage = DEFAULT_MESSAGE;
 
     /**
      * Шаблонное сообщение.
      *
      * @since 3.0.0
+     * @deprecated Используйте {@linkplain #TEMPLATE_MESSAGE}.
      */
-    public static final @NotNls @NotNull String templateMessage = "The %s could not be created";
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.CreationException.TEMPLATE_MESSAGE")
+    public static final @NotNls @NotNull String templateMessage = TEMPLATE_MESSAGE;
 
     /**
-     * Инициализирует исключение по умолчанию.
+     * Создаёт исключение по умолчанию.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("-> new")
     public CreationException() {
-        super(defaultMessage);
+        super(DEFAULT_MESSAGE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением.
+     * Создаёт исключение на основе сообщения.
      *
      * @param message сообщение.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_ -> new")
     public CreationException(final @NotNls @Nullable String message) {
         super(message);
     }
 
     /**
-     * Инициализирует исключение с переопределённой причиной.
+     * Создаёт исключение на основе причины.
      *
      * @param cause причина.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_ -> new")
     public CreationException(final @Nullable Throwable cause) {
         super(cause);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением и причиной.
+     * Создаёт исключение на основе сообщения и причины.
      *
      * @param message сообщение.
      * @param cause причина.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _ -> new")
     public CreationException(final @NotNls @Nullable String message, final @Nullable Throwable cause) {
         super(message, cause);
     }
 
     /**
-     * Инициализирует исключение с переопределённым параметром подавления и трассировки стека.
+     * Создаёт исключение на основе параметров подавления и трассировки стека.
      *
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _ -> new")
     public CreationException(final boolean suppression, final boolean writable) {
         super(suppression, writable);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением, причиной, параметром подавления и трассировки стека.
+     * Создаёт исключение на основе сообщения, параметров подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    public CreationException(final String message, final boolean suppression, final boolean writable) {
+        super(message, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение на основе причины, параметров подавления и трассировки стека.
+     *
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    public CreationException(final Throwable cause, final boolean suppression, final boolean writable) {
+        super(cause, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение на основе сообщения, причины, параметров подавления и трассировки стека.
      *
      * @param message сообщение.
      * @param cause причина.
@@ -103,7 +148,7 @@ public class CreationException extends SoftException {
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _, _, _ -> new")
     protected CreationException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
                                 final boolean suppression, final boolean writable) {
         super(message, cause, suppression, writable);
