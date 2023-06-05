b6ad14b44cfee840aa5dfc10458cf50ff5b12767
Bloogefest <bloogefest@gmail.com>
2023-06-05 12:54:30 +0300

    Изменение полей и конструкторов исключения `SoftException`: `SoftException.defaultMessage`, `SoftException.defaultCause`, `SoftException.defaultSuppression`, `SoftException.defaultWritable`, `SoftException()`, `SoftException(String)`, `SoftException(Throwable)`, `SoftException(String, Throwable)`, `SoftException(boolean, boolean)`, `SoftException(String, Throwable, boolean, boolean)` —, а также введение других: `SoftException.DEFAULT_MESSAGE`, `SoftException.DEFAULT_CAUSE`, `SoftException.DEFAULT_SUPPRESSION`, `SoftException.DEFAULT_WRITABLE`, `SoftException(String, boolean, boolean)`, `SoftException(Throwable, boolean, boolean)`

--- src/main/java/com/bloogefest/common/SoftException.java
+++ src/main/java/com/bloogefest/common/SoftException.java
@@ -6,9 +6,7 @@
 
 package com.bloogefest.common;
 
-import com.bloogefest.annotation.analysis.Contract;
-import com.bloogefest.annotation.analysis.NotNls;
-import com.bloogefest.annotation.analysis.Nullable;
+import com.bloogefest.annotation.analysis.*;
 
 /**
  * Мягкое исключение.
@@ -22,93 +20,165 @@ public class SoftException extends RuntimeException {
     /**
      * Сообщение по умолчанию.
      *
-     * @since 3.0.0
+     * @since 4.0.0-RC3
      */
-    public static final @NotNls @Nullable String defaultMessage = null;
+    public static final @NotNls @Nullable String DEFAULT_MESSAGE = null;
+
+    /**
+     * Причина по умолчанию.
+     *
+     * @since 4.0.0-RC3
+     */
+    public static final @Nullable Throwable DEFAULT_CAUSE = null;
+
+    /**
+     * Параметр подавления по умолчанию.
+     *
+     * @since 4.0.0-RC3
+     */
+    public static final boolean DEFAULT_SUPPRESSION = false;
+
+    /**
+     * Параметр трассировки стека по умолчанию.
+     *
+     * @since 4.0.0-RC3
+     */
+    public static final boolean DEFAULT_WRITABLE = true;
+
+    /**
+     * Сообщение по умолчанию.
+     *
+     * @since 3.0.0
+     * @deprecated Используйте {@linkplain #DEFAULT_MESSAGE}.
+     */
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.SoftError.DEFAULT_MESSAGE")
+    public static final @NotNls @Nullable String defaultMessage = DEFAULT_MESSAGE;
 
     /**
      * Причина по умолчанию.
      *
      * @since 3.0.0
+     * @deprecated Используйте {@linkplain #DEFAULT_CAUSE}.
      */
-    public static final @Nullable Throwable defaultCause = null;
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.SoftException.DEFAULT_CAUSE")
+    public static final @Nullable Throwable defaultCause = DEFAULT_CAUSE;
 
     /**
      * Параметр подавления по умолчанию.
      *
      * @since 3.0.0
+     * @deprecated Используйте {@linkplain #DEFAULT_SUPPRESSION}.
      */
-    public static final boolean defaultSuppression = false;
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.SoftException.DEFAULT_SUPPRESSION")
+    public static final boolean defaultSuppression = DEFAULT_SUPPRESSION;
 
     /**
      * Параметр трассировки стека по умолчанию.
      *
      * @since 3.0.0
+     * @deprecated Используйте {@linkplain #DEFAULT_WRITABLE}.
      */
-    public static final boolean defaultWritable = true;
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.SoftException.DEFAULT_WRITABLE")
+    public static final boolean defaultWritable = DEFAULT_WRITABLE;
 
     /**
-     * Инициализирует исключение по умолчанию.
+     * Создаёт исключение по умолчанию.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("-> new")
     public SoftException() {
-        super(defaultMessage, defaultCause, defaultSuppression, defaultWritable);
+        super(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением.
+     * Создаёт исключение на основе сообщения.
      *
      * @param message сообщение.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_ -> new")
     public SoftException(final @NotNls @Nullable String message) {
-        super(message, defaultCause, defaultSuppression, defaultWritable);
+        super(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённой причиной.
+     * Создаёт исключение на основе причины.
      *
      * @param cause причина.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_ -> new")
     public SoftException(final @Nullable Throwable cause) {
-        super(defaultMessage, cause, defaultSuppression, defaultWritable);
+        super(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением и причиной.
+     * Создаёт исключение на основе сообщения и причины.
      *
      * @param message сообщение.
      * @param cause причина.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _ -> new")
     public SoftException(final @NotNls @Nullable String message, final @Nullable Throwable cause) {
-        super(message, cause, defaultSuppression, defaultWritable);
+        super(message, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым параметром подавления и трассировки стека.
+     * Создаёт исключение на основе параметров подавления и трассировки стека.
      *
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _ -> new")
     public SoftException(final boolean suppression, final boolean writable) {
-        super(defaultMessage, defaultCause, suppression, writable);
+        super(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, writable);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением, причиной, параметром подавления и трассировки стека.
+     * Создаёт исключение на основе сообщения, параметров подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public SoftException(final @NotNls @Nullable String message, final boolean suppression, final boolean writable) {
+        super(message, DEFAULT_CAUSE, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение на основе причины, параметров подавления и трассировки стека.
+     *
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public SoftException(final @Nullable Throwable cause, final boolean suppression, final boolean writable) {
+        super(DEFAULT_MESSAGE, cause, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение на основе сообщения, причины, параметров подавления и трассировки стека.
      *
      * @param message сообщение.
      * @param cause причина.
@@ -117,7 +187,7 @@ public class SoftException extends RuntimeException {
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _, _, _ -> new")
     protected SoftException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
                             final boolean suppression, final boolean writable) {
         super(message, cause, suppression, writable);
