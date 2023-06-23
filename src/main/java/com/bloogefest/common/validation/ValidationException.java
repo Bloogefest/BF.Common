467bb709a1f78e5f78f1f128e858cfe728725741
Bloogefest <bloogefest@gmail.com>
2023-06-23 18:33:59 +0300

    Изменение исключения `ValidationException`

--- src/main/java/com/bloogefest/common/validation/ValidationException.java
+++ src/main/java/com/bloogefest/common/validation/ValidationException.java
@@ -6,10 +6,7 @@
 
 package com.bloogefest.common.validation;
 
-import com.bloogefest.annotation.analysis.Contract;
-import com.bloogefest.annotation.analysis.NotNls;
-import com.bloogefest.annotation.analysis.NotNull;
-import com.bloogefest.annotation.analysis.Nullable;
+import com.bloogefest.annotation.analysis.*;
 import com.bloogefest.common.SoftException;
 
 /**
@@ -22,90 +19,153 @@ public class ValidationException extends SoftException {
     /**
      * Сообщение по умолчанию.
      *
-     * @since 3.0.0
+     * @since 4.0.0-RC3
      */
-    public static final @NotNls @NotNull String defaultMessage = "The object cannot be validated";
+    public static final @NotNls @NotNull String DEFAULT_MESSAGE = "The object cannot be validated";
+
+    /**
+     * Шаблонное сообщение.
+     *
+     * @since 4.0.0-RC3
+     */
+    public static final @NotNls @NotNull String TEMPLATE_MESSAGE = "%s cannot be validated";
+
+    /**
+     * Сообщение по умолчанию.
+     *
+     * @since 3.0.0
+     * @deprecated Используйте {@linkplain #DEFAULT_MESSAGE}.
+     */
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.validation.ValidationException.DEFAULT_MESSAGE")
+    public static final @NotNls @NotNull String defaultMessage = DEFAULT_MESSAGE;
 
     /**
      * Шаблонное сообщение.
      *
      * @since 3.0.0
+     * @deprecated Используйте {@linkplain #TEMPLATE_MESSAGE}.
      */
-    public static final @NotNls @NotNull String templateMessage = "%s cannot be validated";
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.validation.ValidationException.TEMPLATE_MESSAGE")
+    public static final @NotNls @NotNull String templateMessage = TEMPLATE_MESSAGE;
 
     /**
-     * Инициализирует исключение по умолчанию.
+     * Создаёт исключение валидации объекта на основе {@linkplain #DEFAULT_MESSAGE сообщения},
+     * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
-     * @since 1.0.0
+     * @since 4.0.0-RC3
      */
-    @Contract
+    @Contract("-> new")
     public ValidationException() {
-        super(defaultMessage);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением.
+     * Создаёт исключение валидации объекта на основе переданного сообщения, {@linkplain #DEFAULT_CAUSE причины},
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      *
-     * @since 1.0.0
+     * @since 4.0.0-RC3
      */
-    @Contract
+    @Contract("_ -> new")
     public ValidationException(final @NotNls @Nullable String message) {
-        super(message);
+        this(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённой причиной.
+     * Создаёт исключение валидации объекта на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной
+     * причины, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param cause причина.
      *
-     * @since 1.0.0
+     * @since 4.0.0-RC3
      */
-    @Contract
+    @Contract("_ -> new")
     public ValidationException(final @Nullable Throwable cause) {
-        super(cause);
+        this(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением и причиной.
+     * Создаёт исключение валидации объекта на основе переданного сообщения и причины,
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      * @param cause причина.
      *
-     * @since 1.0.0
+     * @since 4.0.0-RC3
      */
-    @Contract
+    @Contract("_, _ -> new")
     public ValidationException(final @NotNls @Nullable String message, final @Nullable Throwable cause) {
-        super(message, cause);
+        this(message, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым параметром подавления и трассировки стека.
+     * Создаёт исключение валидации объекта на основе {@linkplain #DEFAULT_MESSAGE сообщения} и
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
      *
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
-     * @since 3.0.0
+     * @since 4.0.0-RC3
      */
-    @Contract
+    @Contract("_, _ -> new")
     public ValidationException(final boolean suppression, final boolean writable) {
-        super(suppression, writable);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, writable);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением, причиной, параметром подавления и трассировки стека.
+     * Создаёт исключение валидации объекта на основе переданного сообщения,
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public ValidationException(final @NotNls @Nullable String message, final boolean suppression,
+                               final boolean writable) {
+        this(message, DEFAULT_CAUSE, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение валидации объекта на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной
+     * причины, параметров подавления и трассировки стека.
+     *
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public ValidationException(final @Nullable Throwable cause, final boolean suppression, final boolean writable) {
+        this(DEFAULT_MESSAGE, cause, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение валидации объекта на основе переданного сообщения, причины, параметров подавления и
+     * трассировки стека.
      *
      * @param message сообщение.
      * @param cause причина.
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
-     * @since 1.0.0
+     * @since 4.0.0-RC3
      */
-    @Contract
-    protected ValidationException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
-                                  final boolean suppression, final boolean writable) {
+    @Contract("_, _, _, _ -> new")
+    public ValidationException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
+                               final boolean suppression, final boolean writable) {
         super(message, cause, suppression, writable);
     }
 
