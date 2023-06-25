1535bb6881c5a4fd8319b9372310ac96b489db52
Bloogefest <bloogefest@gmail.com>
2023-06-26 00:31:18 +0300

    Изменение исключения `SupplyException`

--- src/main/java/com/bloogefest/common/function/SupplyException.java
+++ src/main/java/com/bloogefest/common/function/SupplyException.java
@@ -6,94 +6,157 @@
 
 package com.bloogefest.common.function;
 
-import com.bloogefest.annotation.analysis.Contract;
-import com.bloogefest.annotation.analysis.NotNls;
-import com.bloogefest.annotation.analysis.NotNull;
-import com.bloogefest.annotation.analysis.Nullable;
+import com.bloogefest.annotation.analysis.*;
 
 /**
  * Исключение поставки объекта.
  *
  * @since 3.0.0
+ * @deprecated Используйте {@linkplain GetException}.
  */
+@Deprecated(since = "4.0.0-RC3", forRemoval = true)
+@Removal("4.0.0-RC4")
+@Obsolete("com.bloogefest.common.function.GetException")
 public class SupplyException extends FunctionException {
 
     /**
      * Сообщение по умолчанию.
      *
-     * @since 3.0.0
+     * @since 4.0.0-RC3
      */
-    public static final @NotNls @NotNull String defaultMessage = "The object could not be supplied";
+    public static final @NotNls @NotNull String DEFAULT_MESSAGE = "Failed to supply the object";
+
+    /**
+     * Шаблонное сообщение.
+     *
+     * @since 4.0.0-RC3
+     */
+    public static final @NotNls @NotNull String TEMPLATE_MESSAGE = "Failed to supply %s";
+
+    /**
+     * Сообщение по умолчанию.
+     *
+     * @since 3.0.0
+     * @deprecated Используйте {@linkplain #DEFAULT_MESSAGE}.
+     */
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.function.SupplyException.DEFAULT_MESSAGE")
+    public static final @NotNls @NotNull String defaultMessage = DEFAULT_MESSAGE;
 
     /**
      * Шаблонное сообщение.
      *
      * @since 3.0.0
+     * @deprecated Используйте {@linkplain #TEMPLATE_MESSAGE}.
      */
-    public static final @NotNls @NotNull String templateMessage = "The %s could not be supplied";
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.function.SupplyException.TEMPLATE_MESSAGE")
+    public static final @NotNls @NotNull String templateMessage = TEMPLATE_MESSAGE;
 
     /**
-     * Инициализирует исключение по умолчанию.
+     * Создаёт исключение поставки объекта на основе {@linkplain #DEFAULT_MESSAGE сообщения},
+     * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("-> new")
     public SupplyException() {
-        super(defaultMessage);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением.
+     * Создаёт исключение поставки объекта на основе переданного сообщения, {@linkplain #DEFAULT_CAUSE причины},
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_ -> new")
     public SupplyException(final @NotNls @Nullable String message) {
-        super(message);
+        this(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённой причиной.
+     * Создаёт исключение поставки объекта на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной
+     * причины, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param cause причина.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_ -> new")
     public SupplyException(final @Nullable Throwable cause) {
-        super(cause);
+        this(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением и причиной.
+     * Создаёт исключение поставки объекта на основе переданного сообщения и причины,
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      * @param cause причина.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _ -> new")
     public SupplyException(final @NotNls @Nullable String message, final @Nullable Throwable cause) {
-        super(message, cause);
+        this(message, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым параметром подавления и трассировки стека.
+     * Создаёт исключение поставки объекта на основе {@linkplain #DEFAULT_MESSAGE сообщения} и
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
      *
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _ -> new")
     public SupplyException(final boolean suppression, final boolean writable) {
-        super(suppression, writable);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, writable);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением, причиной, параметром подавления и трассировки стека.
+     * Создаёт исключение поставки объекта на основе переданного сообщения,
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public SupplyException(final @NotNls @Nullable String message, final boolean suppression, final boolean writable) {
+        this(message, DEFAULT_CAUSE, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение поставки объекта на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной
+     * причины, параметров подавления и трассировки стека.
+     *
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public SupplyException(final @Nullable Throwable cause, final boolean suppression, final boolean writable) {
+        this(DEFAULT_MESSAGE, cause, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение поставки объекта на основе переданного сообщения, причины, параметров подавления и трассировки
+     * стека.
      *
      * @param message сообщение.
      * @param cause причина.
@@ -102,9 +165,9 @@ public class SupplyException extends FunctionException {
      *
      * @since 3.0.0
      */
-    @Contract
-    protected SupplyException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
-                              final boolean suppression, final boolean writable) {
+    @Contract("_, _, _, _ -> new")
+    public SupplyException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
+                           final boolean suppression, final boolean writable) {
         super(message, cause, suppression, writable);
     }
 
