9d71077dd3d61df427dbabfc6253a42a0b408017
Bloogefest <bloogefest@gmail.com>
2023-06-26 13:19:11 +0300

    Изменение исключения `ComputeException`

--- src/main/java/com/bloogefest/common/function/ComputeException.java
+++ src/main/java/com/bloogefest/common/function/ComputeException.java
@@ -4,12 +4,15 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.bloogefest.common.function;
 
-import com.bloogefest.annotation.analysis.Contract;
-import com.bloogefest.annotation.analysis.NotNls;
-import com.bloogefest.annotation.analysis.NotNull;
-import com.bloogefest.annotation.analysis.Nullable;
+import com.bloogefest.annotation.analysis.*;
 
 /**
  * Исключение вычисления функции.
@@ -21,79 +24,141 @@ public class ComputeException extends FunctionException {
     /**
      * Сообщение по умолчанию.
      *
-     * @since 3.0.0
+     * @since 4.0.0-RC3
      */
-    public static final @NotNls @NotNull String defaultMessage = "The function could not be computed";
+    public static final @NotNls @NotNull String DEFAULT_MESSAGE = "Failed to compute the function";
+
+    /**
+     * Шаблонное сообщение.
+     *
+     * @since 4.0.0-RC3
+     */
+    public static final @NotNls @NotNull String TEMPLATE_MESSAGE = "Failed to compute %s";
+
+    /**
+     * Сообщение по умолчанию.
+     *
+     * @since 3.0.0
+     * @deprecated Используйте {@linkplain #DEFAULT_MESSAGE}.
+     */
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.function.FunctionException.DEFAULT_MESSAGE")
+    public static final @NotNls @NotNull String defaultMessage = DEFAULT_MESSAGE;
 
     /**
      * Шаблонное сообщение.
      *
      * @since 3.0.0
+     * @deprecated Используйте {@linkplain #TEMPLATE_MESSAGE}.
      */
-    public static final @NotNls @NotNull String templateMessage = "The %s could not be computed";
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.function.FunctionException.TEMPLATE_MESSAGE")
+    public static final @NotNls @NotNull String templateMessage = TEMPLATE_MESSAGE;
 
     /**
-     * Инициализирует исключение по умолчанию.
+     * Создаёт исключение вычисления функции на основе {@linkplain #DEFAULT_MESSAGE сообщения},
+     * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("-> new")
     public ComputeException() {
-        super(defaultMessage);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением.
+     * Создаёт исключение вычисления функции на основе переданного сообщения, {@linkplain #DEFAULT_CAUSE причины},
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_ -> new")
     public ComputeException(final @NotNls @Nullable String message) {
-        super(message);
+        this(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённой причиной.
+     * Создаёт исключение вычисления функции на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной
+     * причины, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param cause причина.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_ -> new")
     public ComputeException(final @Nullable Throwable cause) {
-        super(cause);
+        this(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением и причиной.
+     * Создаёт исключение вычисления функции на основе переданного сообщения и причины,
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      * @param cause причина.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _ -> new")
     public ComputeException(final @NotNls @Nullable String message, final @Nullable Throwable cause) {
-        super(message, cause);
+        this(message, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым параметром подавления и трассировки стека.
+     * Создаёт исключение вычисления функции на основе {@linkplain #DEFAULT_MESSAGE сообщения} и
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
      *
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _ -> new")
     public ComputeException(final boolean suppression, final boolean writable) {
-        super(suppression, writable);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, writable);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением, причиной, параметром подавления и трассировки стека.
+     * Создаёт исключение вычисления функции на основе переданного сообщения,
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public ComputeException(final @NotNls @Nullable String message, final boolean suppression, final boolean writable) {
+        this(message, DEFAULT_CAUSE, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение вычисления функции на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной
+     * причины, параметров подавления и трассировки стека.
+     *
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public ComputeException(final @Nullable Throwable cause, final boolean suppression, final boolean writable) {
+        this(DEFAULT_MESSAGE, cause, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение вычисления функции на основе переданного сообщения, причины, параметров подавления и
+     * трассировки стека.
      *
      * @param message сообщение.
      * @param cause причина.
@@ -102,9 +167,9 @@ public class ComputeException extends FunctionException {
      *
      * @since 3.0.0
      */
-    @Contract
-    protected ComputeException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
-                               final boolean suppression, final boolean writable) {
+    @Contract("_, _, _, _ -> new")
+    public ComputeException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
+                            final boolean suppression, final boolean writable) {
         super(message, cause, suppression, writable);
     }
 
