830daf82deb98ece460c7a46c9f23c4baff73356
Bloogefest <bloogefest@gmail.com>
2023-06-26 13:19:35 +0300

    Изменение исключения `UncaughtException`

--- src/main/java/com/bloogefest/common/function/UncaughtException.java
+++ src/main/java/com/bloogefest/common/function/UncaughtException.java
@@ -4,6 +4,18 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.bloogefest.common.function;
 
 import com.bloogefest.annotation.analysis.Contract;
@@ -33,67 +45,108 @@ public class UncaughtException extends FunctionException {
     public static final @NotNls @NotNull String TEMPLATE_MESSAGE = "Failed to catch the %s";
 
     /**
-     * Создаёт исключение по умолчанию.
+     * Создаёт исключение ловли исключения либо ошибки на основе {@linkplain #DEFAULT_MESSAGE сообщения},
+     * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @since 4.0.0-RC3
      */
     @Contract("-> new")
     public UncaughtException() {
-        super(DEFAULT_MESSAGE);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт исключение на основе сообщения.
+     * Создаёт исключение ловли исключения либо ошибки на основе переданного сообщения,
+     * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      *
      * @since 4.0.0-RC3
      */
-    @Contract("-> new")
+    @Contract("_ -> new")
     public UncaughtException(final @NotNls @Nullable String message) {
-        super(message);
+        this(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт исключение на основе причины.
+     * Создаёт исключение ловли исключения либо ошибки на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию},
+     * переданной причины, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param cause причина.
      *
      * @since 4.0.0-RC3
      */
-    @Contract("-> new")
+    @Contract("_ -> new")
     public UncaughtException(final @Nullable Throwable cause) {
-        super(cause);
+        this(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт исключение на основе сообщения и причины.
+     * Создаёт исключение ловли исключения либо ошибки на основе переданного сообщения и причины,
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      * @param cause причина.
      *
      * @since 4.0.0-RC3
      */
-    @Contract("-> new")
+    @Contract("_, _ -> new")
     public UncaughtException(final @NotNls @Nullable String message, final @Nullable Throwable cause) {
-        super(message, cause);
+        this(message, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт исключение на основе параметров подавления и трассировки стека.
+     * Создаёт исключение ловли исключения либо ошибки на основе {@linkplain #DEFAULT_MESSAGE сообщения} и
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
      *
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
      * @since 4.0.0-RC3
      */
-    @Contract("-> new")
+    @Contract("_, _ -> new")
     public UncaughtException(final boolean suppression, final boolean writable) {
-        super(suppression, writable);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, writable);
     }
 
     /**
-     * Создаёт исключение на основе сообщения, причины, параметров подавления и трассировки стека.
+     * Создаёт исключение ловли исключения либо ошибки на основе переданного сообщения,
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public UncaughtException(final @NotNls @Nullable String message, final boolean suppression,
+                             final boolean writable) {
+        this(message, DEFAULT_CAUSE, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение ловли исключения либо ошибки на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию},
+     * переданной причины, параметров подавления и трассировки стека.
+     *
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public UncaughtException(final @Nullable Throwable cause, final boolean suppression, final boolean writable) {
+        this(DEFAULT_MESSAGE, cause, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение ловли исключения либо ошибки на основе переданного сообщения, причины, параметров подавления и
+     * трассировки стека.
      *
      * @param message сообщение.
      * @param cause причина.
@@ -102,9 +155,9 @@ public class UncaughtException extends FunctionException {
      *
      * @since 4.0.0-RC3
      */
-    @Contract("-> new")
-    protected UncaughtException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
-                                final boolean suppression, final boolean writable) {
+    @Contract("_, _, _, _ -> new")
+    public UncaughtException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
+                             final boolean suppression, final boolean writable) {
         super(message, cause, suppression, writable);
     }
 
