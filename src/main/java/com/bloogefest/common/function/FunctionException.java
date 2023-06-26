5d7de2c52c0566c44e7e313bfe84974eb26fb831
Bloogefest <bloogefest@gmail.com>
2023-06-26 13:18:50 +0300

    Изменение исключения `FunctionException`

--- src/main/java/com/bloogefest/common/function/FunctionException.java
+++ src/main/java/com/bloogefest/common/function/FunctionException.java
@@ -6,10 +6,7 @@
 
 package com.bloogefest.common.function;
 
-import com.bloogefest.annotation.analysis.Contract;
-import com.bloogefest.annotation.analysis.NotNls;
-import com.bloogefest.annotation.analysis.NotNull;
-import com.bloogefest.annotation.analysis.Nullable;
+import com.bloogefest.annotation.analysis.*;
 import com.bloogefest.common.SoftException;
 
 /**
@@ -22,79 +19,141 @@ public class FunctionException extends SoftException {
     /**
      * Сообщение по умолчанию.
      *
-     * @since 3.0.0
+     * @since 4.0.0-RC3
      */
-    public static final @NotNls @NotNull String defaultMessage = "The function could not be executed";
+    public static final @NotNls @Null String DEFAULT_MESSAGE = null;
+
+    /**
+     * Шаблонное сообщение.
+     *
+     * @since 4.0.0-RC3
+     */
+    public static final @NotNls @Null String TEMPLATE_MESSAGE = null;
+
+    /**
+     * Сообщение по умолчанию.
+     *
+     * @since 3.0.0
+     * @deprecated Используйте {@linkplain #DEFAULT_MESSAGE}.
+     */
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.function.FunctionException.DEFAULT_MESSAGE")
+    public static final @NotNls @Null String defaultMessage = DEFAULT_MESSAGE;
 
     /**
      * Шаблонное сообщение.
      *
      * @since 3.0.0
+     * @deprecated Используйте {@linkplain #TEMPLATE_MESSAGE}.
      */
-    public static final @NotNls @NotNull String templateMessage = "The %s could not be executed";
+    @Deprecated(since = "4.0.0-RC3", forRemoval = true)
+    @Removal("4.0.0-RC4")
+    @Obsolete("com.bloogefest.common.function.FunctionException.TEMPLATE_MESSAGE")
+    public static final @NotNls @Null String templateMessage = TEMPLATE_MESSAGE;
 
     /**
-     * Инициализирует исключение по умолчанию.
+     * Создаёт исключение функции на основе {@linkplain #DEFAULT_MESSAGE сообщения},
+     * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @since 1.0.0
      */
-    @Contract
+    @Contract("-> new")
     public FunctionException() {
-        super(defaultMessage);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением.
+     * Создаёт исключение функции на основе переданного сообщения, {@linkplain #DEFAULT_CAUSE причины},
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      *
      * @since 1.0.0
      */
-    @Contract
+    @Contract("_ -> new")
     public FunctionException(final @NotNls @Nullable String message) {
-        super(message);
+        this(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённой причиной.
+     * Создаёт исключение функции на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной причины,
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param cause причина.
      *
      * @since 1.0.0
      */
-    @Contract
+    @Contract("_ -> new")
     public FunctionException(final @Nullable Throwable cause) {
-        super(cause);
+        this(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением и причиной.
+     * Создаёт исключение функции на основе переданного сообщения и причины,
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      * @param cause причина.
      *
      * @since 1.0.0
      */
-    @Contract
+    @Contract("_, _ -> new")
     public FunctionException(final @NotNls @Nullable String message, final @Nullable Throwable cause) {
-        super(message, cause);
+        this(message, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Инициализирует исключение с переопределённым параметром подавления и трассировки стека.
+     * Создаёт исключение функции на основе {@linkplain #DEFAULT_MESSAGE сообщения} и
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
      *
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
      * @since 3.0.0
      */
-    @Contract
+    @Contract("_, _ -> new")
     public FunctionException(final boolean suppression, final boolean writable) {
-        super(suppression, writable);
+        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, writable);
     }
 
     /**
-     * Инициализирует исключение с переопределённым сообщением, причиной, параметром подавления и трассировки стека.
+     * Создаёт исключение функции на основе переданного сообщения, {@linkplain #DEFAULT_CAUSE причины по умолчанию},
+     * переданных параметров подавления и трассировки стека.
+     *
+     * @param message сообщение.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public FunctionException(final @NotNls @Nullable String message, final boolean suppression,
+                             final boolean writable) {
+        this(message, DEFAULT_CAUSE, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение функции на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной причины,
+     * параметров подавления и трассировки стека.
+     *
+     * @param cause причина.
+     * @param suppression параметр подавления.
+     * @param writable параметр трассировки стека.
+     *
+     * @since 4.0.0-RC3
+     */
+    @Contract("_, _, _ -> new")
+    public FunctionException(final @Nullable Throwable cause, final boolean suppression, final boolean writable) {
+        this(DEFAULT_MESSAGE, cause, suppression, writable);
+    }
+
+    /**
+     * Создаёт исключение функции на основе переданного сообщения, причины, параметров подавления и трассировки стека.
      *
      * @param message сообщение.
      * @param cause причина.
@@ -103,9 +162,9 @@ public class FunctionException extends SoftException {
      *
      * @since 1.0.0
      */
-    @Contract
-    protected FunctionException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
-                                final boolean suppression, final boolean writable) {
+    @Contract("_, _, _, _ -> new")
+    public FunctionException(final @NotNls @Nullable String message, final @Nullable Throwable cause,
+                             final boolean suppression, final boolean writable) {
         super(message, cause, suppression, writable);
     }
 
