713f27d4cde739a90901964b8ae93cf2e5cb9793
Bloogefest <bloogefest@gmail.com>
2023-12-09 20:51:21 +0300

    Переработка исключения создания объекта класса-утилиты

--- src/main/java/com/bloogefest/common/creation/UtilityException.java
+++ src/main/java/com/bloogefest/common/creation/UtilityException.java
@@ -11,99 +11,106 @@ import com.bloogefest.annotation.NonNull;
 import com.bloogefest.annotation.Nullable;
 
 /**
- * Исключение создания объекта утилитарного типа.
+ * Данный класс представляет собой исключение создания объекта класса-утилиты и применяется в некритических ситуациях,
+ * не требующих немедленного реагирования. Для этого он расширяет класс исключения создания объекта, а также объявляет и
+ * реализует наиболее полезные конструкторы и статические поля.
  *
- * @since 3.0.0
+ * @apiNote Исключение создания объекта класса-утилиты обычно возникает при любой попытке создания объекта
+ * класса-утилиты.
+ * @since 3.0.0-RC1
  */
 public class UtilityException extends CreationException {
 
     /**
-     * Сообщение по умолчанию.
+     * Данное неизменяемое статическое поле представляет собой ненулевой объект класса строки и содержит сообщение
+     * исключения создания объекта класса-утилиты по умолчанию.
      *
      * @since 4.0.0-RC3
      */
-    public static final @NonNull String DEFAULT_MESSAGE = "The utility object must not be created";
+    public static final @NonNull String DEFAULT_MESSAGE = "The utility class object must not be created";
 
     /**
-     * Шаблонное сообщение.
+     * Данное неизменяемое статическое поле представляет собой ненулевой объект класса строки и содержит шаблонное
+     * сообщение исключения создания объекта класса-утилиты.
      *
      * @since 4.0.0-RC3
      */
-    public static final @NonNull String TEMPLATE_MESSAGE = "The %s must not be created";
+    public static final @NonNull String TEMPLATE_MESSAGE = "%s must not be created";
 
     /**
-     * Создаёт исключение создания объекта утилитарного типа на основе {@linkplain #DEFAULT_MESSAGE сообщения},
-     * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * Данный конструктор создаёт исключение создания объекта класса-утилиты на основе
+     * {@linkplain #DEFAULT_MESSAGE сообщения}, {@linkplain #DEFAULT_CAUSE причины},
+     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
      * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("-> new")
+    @Contract(value = "-> new", impact = Contract.Impact.INTERNAL)
     public UtilityException() {
-        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
+        super(DEFAULT_MESSAGE);
     }
 
     /**
-     * Создаёт исключение создания объекта утилитарного типа на основе переданного сообщения,
+     * Данный конструктор создаёт исключение создания объекта класса-утилиты на основе переданного сообщения, а также
      * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
      * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("_ -> new")
+    @Contract(value = "? -> new", impact = Contract.Impact.INTERNAL)
     public UtilityException(final @Nullable String message) {
-        this(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
+        super(message);
     }
 
     /**
-     * Создаёт исключение создания объекта утилитарного типа на основе
-     * {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной причины,
-     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * Данный конструктор создаёт исключение создания объекта класса-утилиты на основе переданной причины, а также
+     * {@linkplain #DEFAULT_MESSAGE сообщения}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
      * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param cause причина.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("_ -> new")
+    @Contract(value = "? -> new", impact = Contract.Impact.INTERNAL)
     public UtilityException(final @Nullable Throwable cause) {
-        this(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
+        super(DEFAULT_MESSAGE, cause);
     }
 
     /**
-     * Создаёт исключение создания объекта утилитарного типа на основе переданного сообщения и причины,
-     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * Данный конструктор создаёт исключение создания объекта класса-утилиты на основе переданного сообщения и причины,
+     * а также {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
      * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      * @param cause причина.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("_, _ -> new")
+    @Contract(value = "?, ? -> new", impact = Contract.Impact.INTERNAL)
     public UtilityException(final @Nullable String message, final @Nullable Throwable cause) {
-        this(message, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
+        super(message, cause);
     }
 
     /**
-     * Создаёт исключение создания объекта утилитарного типа на основе {@linkplain #DEFAULT_MESSAGE сообщения} и
-     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
+     * Данный конструктор создаёт исключение создания объекта класса-утилиты на основе переданных параметров подавления
+     * и трассировки стека, а также {@linkplain #DEFAULT_MESSAGE сообщения} и
+     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}.
      *
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("_, _ -> new")
+    @Contract(value = "?, ? -> new", impact = Contract.Impact.INTERNAL)
     public UtilityException(final boolean suppression, final boolean writable) {
-        this(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, writable);
+        super(DEFAULT_MESSAGE, suppression, writable);
     }
 
     /**
-     * Создаёт исключение создания объекта утилитарного типа на основе переданного сообщения,
-     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
+     * Данный конструктор создаёт исключение создания объекта класса-утилиты на основе переданного сообщения, параметров
+     * подавления и трассировки стека, а также {@linkplain #DEFAULT_CAUSE причины по умолчанию}.
      *
      * @param message сообщение.
      * @param suppression параметр подавления.
@@ -111,15 +118,14 @@ public class UtilityException extends CreationException {
      *
      * @since 4.0.0-RC3
      */
-    @Contract("_, _, _ -> new")
+    @Contract(value = "?, ?, ? -> new", impact = Contract.Impact.INTERNAL)
     public UtilityException(final @Nullable String message, final boolean suppression, final boolean writable) {
-        this(message, DEFAULT_CAUSE, suppression, writable);
+        super(message, suppression, writable);
     }
 
     /**
-     * Создаёт исключение создания объекта утилитарного типа на основе
-     * {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной причины, параметров подавления и трассировки
-     * стека.
+     * Данный конструктор создаёт исключение создания объекта класса-утилиты на основе переданной причины, параметров
+     * подавления и трассировки стека, а также {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}.
      *
      * @param cause причина.
      * @param suppression параметр подавления.
@@ -127,27 +133,26 @@ public class UtilityException extends CreationException {
      *
      * @since 4.0.0-RC3
      */
-    @Contract("_, _, _ -> new")
+    @Contract(value = "?, ?, ? -> new", impact = Contract.Impact.INTERNAL)
     public UtilityException(final @Nullable Throwable cause, final boolean suppression, final boolean writable) {
-        this(DEFAULT_MESSAGE, cause, suppression, writable);
+        super(DEFAULT_MESSAGE, cause, suppression, writable);
     }
 
     /**
-     * Создаёт исключение создания объекта утилитарного типа на основе переданного сообщения, причины, параметров
-     * подавления и трассировки стека.
+     * Данный конструктор создаёт исключение создания объекта класса-утилиты на основе переданного сообщения, причины,
+     * параметров подавления и трассировки стека.
      *
      * @param message сообщение.
      * @param cause причина.
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("_, _, _, _ -> new")
+    @Contract(value = "?, ?, ?, ? -> new", impact = Contract.Impact.INTERNAL)
     public UtilityException(final @Nullable String message, final @Nullable Throwable cause, final boolean suppression,
                             final boolean writable) {
         super(message, cause, suppression, writable);
     }
-
 
 }
