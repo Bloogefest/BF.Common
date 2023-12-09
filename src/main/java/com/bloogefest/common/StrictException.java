01b160699322c7e6e2cd6fb2b7a088dde331da53
Bloogefest <bloogefest@gmail.com>
2023-12-09 20:01:50 +0300

    Переработка класса строгого исключения

--- src/main/java/com/bloogefest/common/StrictException.java
+++ src/main/java/com/bloogefest/common/StrictException.java
@@ -7,117 +7,124 @@
 package com.bloogefest.common;
 
 import com.bloogefest.annotation.Contract;
+import com.bloogefest.annotation.Null;
 import com.bloogefest.annotation.Nullable;
 
 /**
- * Строгое исключение.
+ * Данный класс представляет собой строгое исключение и применяется в некритических ситуациях, требующих немедленного
+ * реагирования. Для этого он расширяет класс исключения, а также объявляет и реализует наиболее полезные конструкторы и
+ * статические поля.
  *
- * @apiNote Оповещает о несерьёзной проблеме, которую рекомендуется обработать для дальнейшего корректного выполнения
- * программы.
- * @since 3.0.0
+ * @apiNote После возникновения и обработки строгого исключения обычно можно продолжить работу. Например, когда
+ * происходит неожиданное закрытие файлового потока.
+ * @since 3.0.0-RC1
  */
 public class StrictException extends Exception {
 
     /**
-     * Сообщение по умолчанию.
+     * Данное неизменяемое статическое поле представляет собой нулевой объект класса строки и содержит сообщение
+     * строгого исключения по умолчанию.
      *
      * @since 4.0.0-RC3
      */
-    public static final @Nullable String DEFAULT_MESSAGE = null;
+    public static final @Null String DEFAULT_MESSAGE = null;
 
     /**
-     * Причина по умолчанию.
+     * Данное неизменяемое статическое поле представляет собой нулевой объект класса бросаемого и содержит причину
+     * строгого исключения по умолчанию.
      *
      * @since 4.0.0-RC3
      */
-    public static final @Nullable Throwable DEFAULT_CAUSE = null;
+    public static final @Null Throwable DEFAULT_CAUSE = null;
 
     /**
-     * Параметр подавления по умолчанию.
+     * Данное неизменяемое статическое поле представляет собой логическое значение и содержит параметр подавления
+     * строгого исключения по умолчанию.
      *
      * @since 4.0.0-RC3
      */
     public static final boolean DEFAULT_SUPPRESSION = false;
 
     /**
-     * Параметр трассировки стека по умолчанию.
+     * Данное неизменяемое статическое поле представляет собой логическое значение и содержит параметр трассировки стека
+     * строгого исключения по умолчанию.
      *
      * @since 4.0.0-RC3
      */
     public static final boolean DEFAULT_WRITABLE = true;
 
     /**
-     * Создаёт строгое исключение на основе {@linkplain #DEFAULT_MESSAGE сообщения},
+     * Данный конструктор создаёт строгое исключение на основе {@linkplain #DEFAULT_MESSAGE сообщения},
      * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
      * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("-> new")
+    @Contract(value = "-> new", impact = Contract.Impact.INTERNAL)
     public StrictException() {
         super(DEFAULT_MESSAGE, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт строгое исключение на основе переданного сообщения, {@linkplain #DEFAULT_CAUSE причины},
-     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * Данный конструктор создаёт строгое исключение на основе переданного сообщения, а также
+     * {@linkplain #DEFAULT_CAUSE причины}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
      * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("_ -> new")
+    @Contract(value = "? -> new", impact = Contract.Impact.INTERNAL)
     public StrictException(final @Nullable String message) {
         super(message, DEFAULT_CAUSE, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт строгое исключение на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной причины,
-     * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
+     * Данный конструктор создаёт строгое исключение на основе переданной причины, а также
+     * {@linkplain #DEFAULT_MESSAGE сообщения}, {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
      * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param cause причина.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("_ -> new")
+    @Contract(value = "? -> new", impact = Contract.Impact.INTERNAL)
     public StrictException(final @Nullable Throwable cause) {
         super(DEFAULT_MESSAGE, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт строгое исключение на основе переданного сообщения и причины,
+     * Данный конструктор создаёт строгое исключение на основе переданного сообщения и причины, а также
      * {@linkplain #DEFAULT_SUPPRESSION параметров подавления} и
      * {@linkplain #DEFAULT_WRITABLE трассировки стека по умолчанию}.
      *
      * @param message сообщение.
      * @param cause причина.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("_, _ -> new")
+    @Contract(value = "?, ? -> new", impact = Contract.Impact.INTERNAL)
     public StrictException(final @Nullable String message, final @Nullable Throwable cause) {
         super(message, cause, DEFAULT_SUPPRESSION, DEFAULT_WRITABLE);
     }
 
     /**
-     * Создаёт строгое исключение на основе {@linkplain #DEFAULT_MESSAGE сообщения} и
-     * {@linkplain #DEFAULT_CAUSE причины по умолчанию}, переданных параметров подавления и трассировки стека.
+     * Данный конструктор создаёт строгое исключение на основе переданных параметров подавления и трассировки стека, а
+     * также {@linkplain #DEFAULT_MESSAGE сообщения} и {@linkplain #DEFAULT_CAUSE причины по умолчанию}.
      *
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("_, _ -> new")
+    @Contract(value = "?, ? -> new", impact = Contract.Impact.INTERNAL)
     public StrictException(final boolean suppression, final boolean writable) {
         super(DEFAULT_MESSAGE, DEFAULT_CAUSE, suppression, writable);
     }
 
     /**
-     * Создаёт строгое исключение на основе переданного сообщения, {@linkplain #DEFAULT_CAUSE причины по умолчанию},
-     * переданных параметров подавления и трассировки стека.
+     * Данный конструктор создаёт строгое исключение на основе переданного сообщения, параметров подавления и
+     * трассировки стека, а также {@linkplain #DEFAULT_CAUSE причины по умолчанию}.
      *
      * @param message сообщение.
      * @param suppression параметр подавления.
@@ -125,14 +132,14 @@ public class StrictException extends Exception {
      *
      * @since 4.0.0-RC3
      */
-    @Contract("_, _, _ -> new")
+    @Contract(value = "?, ?, ? -> new", impact = Contract.Impact.INTERNAL)
     public StrictException(final @Nullable String message, final boolean suppression, final boolean writable) {
         super(message, DEFAULT_CAUSE, suppression, writable);
     }
 
     /**
-     * Создаёт строгое исключение на основе {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}, переданной причины,
-     * параметров подавления и трассировки стека.
+     * Данный конструктор создаёт строгое исключение на основе переданной причины, параметров подавления и трассировки
+     * стека, а также {@linkplain #DEFAULT_MESSAGE сообщения по умолчанию}.
      *
      * @param cause причина.
      * @param suppression параметр подавления.
@@ -140,22 +147,23 @@ public class StrictException extends Exception {
      *
      * @since 4.0.0-RC3
      */
-    @Contract("_, _, _ -> new")
+    @Contract(value = "?, ?, ? -> new", impact = Contract.Impact.INTERNAL)
     public StrictException(final @Nullable Throwable cause, final boolean suppression, final boolean writable) {
         super(DEFAULT_MESSAGE, cause, suppression, writable);
     }
 
     /**
-     * Создаёт строгое исключение на основе переданного сообщения, причины, параметров подавления и трассировки стека.
+     * Данный конструктор создаёт строгое исключение на основе переданного сообщения, причины, параметров подавления и
+     * трассировки стека.
      *
      * @param message сообщение.
      * @param cause причина.
      * @param suppression параметр подавления.
      * @param writable параметр трассировки стека.
      *
-     * @since 3.0.0
+     * @since 3.0.0-RC1
      */
-    @Contract("_, _, _, _ -> new")
+    @Contract(value = "?, ?, ?, ? -> new", impact = Contract.Impact.INTERNAL)
     public StrictException(final @Nullable String message, final @Nullable Throwable cause, final boolean suppression,
                            final boolean writable) {
         super(message, cause, suppression, writable);
